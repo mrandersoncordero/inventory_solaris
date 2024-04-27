65313447282787b555e3081ef86dfe3604330ab6
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-04-27 18:19:28 -0400

    model utils

---
+++ inventory/utils/models.py
@@ -0,0 +1,29 @@
+"""Django models utilities."""
+
+# Django
+from django.db import models
+
+class InventorySolaris(models.Model):
+    """InventorySolaris base model.
+      + created (DateTime): Store the datetime the objects was created.
+      + modified (DateTime): Store the datetime the objects was modified
+    """
+
+    created = models.DateTimeField(
+        'created',
+        auto_now_add=True,
+        help_text='Date time on which the object was created'
+    )
+
+    updated = models.DateTimeField(
+        'updated',
+        auto_now=True,
+        help_text='Date time on which the object was last updated'
+    )
+
+    class Meta:
+        """Meta options."""
+        abstract = True
+
+        get_latest_by = 'created'
+        ordering = ['-created', '-updated']
