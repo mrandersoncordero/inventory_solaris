0c05f5f28cbf2eff6b0d68e22394f1504c5b9403
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-04-28 11:09:17 -0400

    sin vercel

--- inventory/settings.py
+++ inventory/settings.py
@@ -90,9 +90,13 @@ WSGI_APPLICATION = 'inventory.wsgi.app'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
 
