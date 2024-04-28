7b870db2be0a834d3cf69a6937e8928382fbdeae
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-04-28 09:09:13 -0400

    static

--- inventory/settings.py
+++ inventory/settings.py
@@ -138,6 +138,8 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
